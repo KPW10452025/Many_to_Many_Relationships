cee95b667c62e8ed32468d98912a7fce11d9ffa0
PoWei <10452025@mail.ntou.edu.tw>
2021-08-16 09:04:50 +0900

    Create Association Table

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Reduce memory usage.
 
 db = SQLAlchemy(app) # Default syntax in SQLAlachemy.
 
+# Use db.Table to create a table. subs means subscriptions. 
+subs = db.Table("subs",
+    db.Column("user_id", db.Integer, db.ForeignKey(user.user_id)),
+    db.Column("channel_id", db.Integer, db.ForeignKey(channel.channel_id))
+    )
+# db.Table("Table_Name", db.Column("Column_Name", configs..., ForeignKey(Table_Name.Column_Name)), ...)
+
 # Create a table named User. db.Model is default syntax in flask_sqlalchemy.
 class User(db.Model):
     __tablename__ = "user" # It's ok not to  code is line.
@@ -20,3 +27,20 @@ class User(db.Model):
 class Channel(db.Model):
     channel_id = db.Column(db.Integer, primary_key=True)
     channel_name = db.Column(db.String(30))
+
+# The goal is connecting these two tablas. 
+# I want to enable one user have many channels that belong to.
+# And then one channel could have many users that are subscribed to that channel.
+# To do that I need to create some kind of "Association Table."
+# The Association Table will have two columns in it.
+# One Column will be a user ID and the other column will be a channel ID.
+# Every row represents a relationship between the two.
+
+# The Association Table could be another model like User and Channel.
+# But since I'm not really going to directly manipulate this table.
+# It's better to create it just as a standalone table
+# So it's not actually part of your model in a sense.
+# But it is in the code and it's represented.
+# I won't be able to directly modify it in my code.
+# SQLAlchemy will handle updating this table.
+# I'll just use db.Table to create a table.
